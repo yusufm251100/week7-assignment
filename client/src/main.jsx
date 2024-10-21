1c68b929f1755268a1ff278877bb5745a2b863d5
yusufm251100 <yusufmahmood00@gmail.com>
2024-10-21 12:11:13 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css"; // Ensure this file exists
+import "./index.css"; 
 
-console.log("Main entry point running"); // Log here
+console.log("Main entry point running"); 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
